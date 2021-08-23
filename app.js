f25f073dec8469506c539e302997aece389703dc
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-08-23 16:51:38 -0700

    created addMember function

--- app.js
+++ app.js
@@ -6,7 +6,34 @@ const Manager = require("./lib/Manager");
 
 const employees = [];
 
+//Initiate the app
 function initApp() {
     startHtml();
     addMember();
+}
+
+// Add a new team member
+function addMember() {
+    inquirer.prompt([{
+        message: "Please enter the team member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select team member's role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "Enter team member's id",
+        name: "id"
+    },
+    {
+        message: "Enter team member's email address",
+        name: "email"
+    }])
 }
