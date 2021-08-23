1f3e9265e5adcc5e29a3dc5956bf7bdde2abcf7c
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-08-23 16:44:36 -0700

    added inital variables

---
+++ app.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const employees = [];
+
+function initApp() {
+    startHtml();
+    addMember();
+}
