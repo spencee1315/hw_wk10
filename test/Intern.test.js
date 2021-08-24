23551f06d7bce1e2933e8ab75d2d8c442cad2d0a
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-08-23 18:11:48 -0700

    defined intern class, tests passed

---
+++ test/Intern.test.js
@@ -0,0 +1,20 @@
+
+const Intern = require("../lib/Intern");
+
+test("Can set school via constructor", () => {
+  const testValue = "USD";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Foo", 1, "test@test.com", "USD");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "USD";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
